253a0fb9ca5bcbeda0e2ffb23368c2bf4155fe85
rys-sa <a578da18-7f35-4604-847f-961f7327b01c@users.tedomum.net>
2024-06-28 22:40:58 +0200

    styled header works

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,9 +1,10 @@
-
+import "../../assets/style/Footer.scss";
 
 function Footer() {
     return (
-        <footer>
-            <p>&copy;2020 Kasa. All rights reserved</p>
+        <footer className="footer">
+            <img className="footer__logo" src={ "" } alt="Logo de Kasa" />
+            <p className="footer__copyright">&copy;2020 Kasa. All rights reserved</p>
         </footer>
     );
 }
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,16 +1,27 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import styled from "styled-components";
+
+// Import style
 import logo from "/logo-kasa.svg";
+import "../../assets/style/Header.scss";
 
-
+const HiddenTitle = styled.h1`
+    display: none;
+`;
 
 function Header() {
+
+    const path = useLocation();
+    console.log(path);
+
     return(
-        <header>
-            <img src={ logo } alt="logo de Kasa" />
-            <h1>Kasa</h1>
-            <nav>
-                <Link to="/">Accueil</Link>
-                <Link to="/about">À propos</Link>
+
+        <header className="header">
+            <img className="header__logo" src={ logo } alt="logo de Kasa" />
+            <HiddenTitle>Kasa</HiddenTitle>
+            <nav className="header__menu">
+                <Link className={`header__menu__link${ path.pathname === "/" ? " header__menu__link--selected" : "" }`} to="/">Accueil</Link>
+                <Link className={`header__menu__link${ path.pathname === "/about" ? " header__menu__link--selected" : ""}`} to="/about">À propos</Link>
             </nav>
 
         </header>
