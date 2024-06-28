deb758c634865a88add2b6c3106fdaa9af4bd745
rys-sa <a578da18-7f35-4604-847f-961f7327b01c@users.tedomum.net>
2024-06-29 00:00:53 +0200

    styled home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,14 +1,37 @@
 import { Link } from "react-router-dom";
-
+import "../../assets/style/Home.scss";
+import homeLandscape from "../../assets/images/home-landscape.jpg";
 
 function Home() {
     return(
-        <>
-        <h2>Accueil</h2>
-        <Link to="/location">Location</Link>
-        <br />
-        <Link to="/location/3">Location 3</Link>
-        </>
+        <main className="home">
+            <div className="home__title-wrap">
+                <img className="home__title-wrap__img" src={ homeLandscape } alt="paysage de falaise de bord de mer" />
+                <h2 className="home__title-wrap__title">Chez vous, partout et ailleurs</h2>
+            </div>
+            <div className="home__location-wrapper">
+                <Link to="/location/3">
+                <article className="home__location-wrapper__card">
+                    <h3 className="home__location-wrapper__card__title"><span>Titre de la location</span></h3>
+                </article>
+                </Link>
+                <Link to="/location/3">
+                <article className="home__location-wrapper__card">
+                    <h3 className="home__location-wrapper__card__title"><span>Titre de la location</span></h3>
+                </article>
+                </Link>
+                <Link to="/location/3">
+                <article className="home__location-wrapper__card">
+                    <h3 className="home__location-wrapper__card__title"><span>Titre de la location</span></h3>
+                </article>
+                </Link>
+                <Link to="/location/3">
+                <article className="home__location-wrapper__card">
+                    <h3 className="home__location-wrapper__card__title"><span>Titre de la location</span></h3>
+                </article>
+                </Link>
+            </div>
+        </main>
     );
 }
 
