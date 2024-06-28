84da9029847476dc2d164aeb197f95cefd0ae7e8
rys-sa <a578da18-7f35-4604-847f-961f7327b01c@users.tedomum.net>
2024-06-28 22:53:23 +0200

    styled footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,9 +1,10 @@
 import "../../assets/style/Footer.scss";
+import logo from "/logo-kasa-white.svg";
 
 function Footer() {
     return (
         <footer className="footer">
-            <img className="footer__logo" src={ "" } alt="Logo de Kasa" />
+            <img className="footer__logo" src={ logo } alt="Logo de Kasa" />
             <p className="footer__copyright">&copy;2020 Kasa. All rights reserved</p>
         </footer>
     );
