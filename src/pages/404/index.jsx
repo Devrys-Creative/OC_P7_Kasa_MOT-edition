6b44cd232d606e40a3971575215088f70be5273f
rys-sa <a578da18-7f35-4604-847f-961f7327b01c@users.tedomum.net>
2024-06-30 16:55:57 +0200

    404 page responsive

--- src/pages/404/index.jsx
+++ src/pages/404/index.jsx
@@ -1,8 +1,13 @@
-
+import { Link } from "react-router-dom";
+import "../../assets/style/404.scss";
 
 function Error404() {
     return (
-        <h2>Erreur 404</h2>
+        <main class='err404'>
+            <h2 className="err404__title">404</h2>
+            <h3 className="err404__subtitle">Oups ! La page que vous demandez n&apos;existe pas.</h3>
+            <Link className="err404__link" to='/'>Retourner sur la page d&apos;accueil</Link>
+        </main>
     );
 }
 
