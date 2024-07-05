7e95de1760c7048189b37740ec743e37397e58b1
rys-sa <a578da18-7f35-4604-847f-961f7327b01c@users.tedomum.net>
2024-07-05 15:54:45 +0200

    preloading Carousel images

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -3,7 +3,19 @@ import { useState } from "react";
 import "../../assets/style/Carousel.scss";
 import arrowRight from "../../assets/images/carousel-arrow-right.svg";
 
+function preloadImages(imgList) {
+    for(let img of imgList) {
+        let preloadedImg = new Image();
+        preloadedImg.src = img;
+    }
+}
+
+
 function Carousel({ picturesList, className = "" }) {
+
+
+    // preload img
+    preloadImages(picturesList);
 
     const picturesKeyMax = picturesList.length - 1;
     const [pictureKey, setpictureKey ] = useState(0);
